38ec9b784923414ae8d5eb3b4a1fa8a11ebb9755
林佳志乃 <twinklestaryoshino@gmail.com>
2018-11-10 12:08:44 +0900

    definitio assosiation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   attr_accessor :group_key
 
+  belongs_to :group
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 end
