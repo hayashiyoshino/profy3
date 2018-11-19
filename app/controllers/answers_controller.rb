a963f7dc735cd17bfa8917ae5f25ceefb5fda811
林佳志乃 <twinklestaryoshino@gmail.com>
2018-11-19 14:15:05 +0900

    definition create action and strong parameters

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,4 +5,15 @@ class AnswersController < ApplicationController
     @answer.question_id = @question.id
     # 回答を新規登録する際には、その回答に対王するquestionのidも関連づけて保存したいので、@answerのプロパティ値としてセットしておく。
   end
+
+  def create
+    @answer = Answer.create(create_params)
+  end
+
+
+
+  private
+  def create_params
+    params.require(:answer).permit(:question_id, :text).merge(user_id: current_user.id)
+  end
 end
